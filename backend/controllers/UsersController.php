bff2c83149aa75abd2aafdd79ea2c3e69bd75876
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-12-06 20:56:42 +0200

    rout to signup function

--- backend/controllers/UsersController.php
+++ backend/controllers/UsersController.php
@@ -13,6 +13,21 @@ class UsersController{
 
 
     function processRequest($method,$data){
+        if($method== "POST")
+        {
+            if(count($data)== 6)
+            $this->signup($data);
+
+            else{
+                http_response_code(404);
+                echo json_encode(["error" => "Data is not valid"]);
+
+            }
+        }
+
+    }
+
+    function signup($data): void{
 
     }
 
