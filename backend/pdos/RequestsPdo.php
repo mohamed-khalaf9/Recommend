78115fc678558d5c5861ae3ba8a1f59243bfeec8
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-12-07 15:27:18 +0200

    insert request row in requests table

--- backend/pdos/RequestsPdo.php
+++ backend/pdos/RequestsPdo.php
@@ -11,6 +11,20 @@ class RequestsPdo{
         
     }
 
+    public function createRequest(int $userId, int $circleId, string $status, string $createdAt): bool
+{
+    $query = "INSERT INTO requests (user_id, circle_id, status, created_at) VALUES (:userId, :circleId, :status, :createdAt)";
+    $stmt = $this->pdo->prepare($query);
+
+    $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
+    $stmt->bindParam(':circleId', $circleId, PDO::PARAM_INT);
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+    $stmt->bindParam(':createdAt', $createdAt, PDO::PARAM_STR);
+
+    return $stmt->execute();
+}
+
+
 }
 
 
