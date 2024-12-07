71d7c45c2b17b304b721c60cdd741985e648f026
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-12-07 14:40:50 +0200

    insert memebers info in members table through this method

--- backend/pdos/MembersPdo.php
+++ backend/pdos/MembersPdo.php
@@ -9,6 +9,26 @@ class MembersPdo{
         
     }
 
+    function createMember($userId,$circleId,$role,$createdAt)
+    {
+        $sql = "INSERT INTO members (userId, circleId, role, createdAt) VALUES (:userId, :circleId, :role, :createdAt)";
+
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                ':userId' => $userId,
+                ':circleId' => $circleId,
+                ':role' => $role,
+                ':createdAt' => $createdAt
+            ]);
+    
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+
+    }
+
 }
 
 
