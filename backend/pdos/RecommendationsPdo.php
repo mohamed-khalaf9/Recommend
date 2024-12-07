b1d9a007f489c6bd8370dde3f8f5b9f5bf20001c
roqia <elmaghrabyroqia2003@gmail.com>
2024-12-07 18:31:38 +0200

    feat:Add create_recommendations function that accepts title,link,descreption..etc  and it returns boolean variable deticting success/failure of creation

--- backend/pdos/RecommendationsPdo.php
+++ backend/pdos/RecommendationsPdo.php
@@ -9,6 +9,22 @@ class RecommendationsPdo{
         $this->pdo=$pdo;
         
     }
+    function create_recommendations(string $title ,string $description,string $link,string $date,int $userId,int $circleId):bool{
+      $sql="INSERT INTO recommendations (title,description,link,userId,circleId,createdAt)VALUES
+      (:title,:descreption,:link,:userId,:circleId,:createdAt)";
+      $stm=$this->pdo->prepare($sql);
+      return $stm->execute([
+        ':title' => $title,
+        ':description' => $description,
+        ':link'=> $link,
+        ':userId' => $userId,
+        ':circleId' => $circleId,
+        ':createdAt' => $date
+      ]
+      );
+      
+
+    }
 
 }
 
