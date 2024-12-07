b844dd76868a6552f7ce29366bdae14d550f4630
roqia <elmaghrabyroqia2003@gmail.com>
2024-12-07 22:43:26 +0200

    feat:Add is_member function to check if user is a member of this circle or not

--- backend/pdos/MembersPdo.php
+++ backend/pdos/MembersPdo.php
@@ -8,6 +8,12 @@ class MembersPdo{
         $this->pdo=$pdo;
         
     }
+    function is_member($userId):bool{
+        $sql="SELECT userId FROM members WHERE userId=:userId";
+        $stm=$this->pdo->prepare($sql);
+        $stm->execute([':userId' => $userId]);
+        return $stm->fetchColumn()!==false;
+      }
 
 }
 
