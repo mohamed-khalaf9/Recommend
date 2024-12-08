35b70e74ca2a4778a3635051077e11d0afa43e5d
roqia <elmaghrabyroqia2003@gmail.com>
2024-12-08 17:42:29 +0200

    feat:Add get_circle_members function and Edit processRequest function by handling conditional statements

--- backend/controllers/MembersController.php
+++ backend/controllers/MembersController.php
@@ -1,13 +1,16 @@
 <?php
  include_once 'pdos/MembersPdo.php';
+ include_once 'controllers/CirclesController.php';
+ include_once 'httpResponse.php';
 class MembersController{
     private $membersPdo;
-
+    private $circleController;
     function __construct()
     {
         $db = new Database();
         $pdo = $db->getConnection();
         $this->membersPdo = new MembersPdo($pdo);
+        $this->circleController=new CirclesController();
     }
 
     public function is_member($userId):bool{
@@ -16,7 +19,27 @@ class MembersController{
     
     
     function processRequest($method,$userId,$id,$data){
-
+     if($id==null){
+        HttpResponse::send( 400,null,["errror" =>"circle id is required"]);
+        return;
+     }
+     if(!($this->circleController->is_exist($id))){
+        HttpResponse::send(404,null,["error"=>"Circle is not found"]);
+     }
+     else{
+        if($method=="GET"){
+            $this->get_circle_members();
+        }
+     }
+    }
+    public function get_circle_members(){
+        $members=$this->membersPdo->get_cirle_members();
+        if(empty($members)){
+            HttpResponse::send(404,null,["error"=>"No members for that circle until now"]);
+        }
+        else{
+            HttpResponse::send(200,null,["members "=>$members]);
+        }
     }
    
 
