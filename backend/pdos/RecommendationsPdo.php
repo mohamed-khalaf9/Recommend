33d9aef5bc9195349cb8d780644ac43c51004b10
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-12-08 19:40:59 +0200

    solve some errors

--- backend/pdos/RecommendationsPdo.php
+++ backend/pdos/RecommendationsPdo.php
@@ -11,12 +11,12 @@ class RecommendationsPdo{
     }
     
     public function create_recommendations(string $title ,string $brief,string $link,string $date,int $userId,int $circleId):bool{
-      $sql="INSERT INTO recommendations (title,brief,link,userId,circleId,createdAt)VALUES
-      (:title, :brief, :link,  :userId, :circleId, :createdAt)";
+      $sql="INSERT INTO recommendations (title,description,link,userId,circleId,createdAt)VALUES
+      (:title, :description, :link,  :userId, :circleId, :createdAt)";
       $stm=$this->pdo->prepare($sql);
       return $stm->execute([
         ':title' => $title,
-        ':brief' => $brief,
+        ':description' => $brief,
         ':link'=> $link,
         ':userId' => $userId,
         ':circleId' => $circleId,
