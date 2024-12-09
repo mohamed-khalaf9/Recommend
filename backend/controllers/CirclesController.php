03fd97e59dedb74833345168646c0b02e565df38
roqia <elmaghrabyroqia2003@gmail.com>
2024-12-10 01:08:12 +0200

    fix:Ensure that who deleting the circle is the admin by calling get_member_role function in member controller

--- backend/controllers/CirclesController.php
+++ backend/controllers/CirclesController.php
@@ -1,27 +1,33 @@
 <?php
 
 include_once 'pdos/CirclesPdo.php';
+include_once 'MembersController.php';
 class CirclesController{
     private $circlesPdo;
-
+    private $memberController;
     function __construct()
     {
         $db = new Database();
         $pdo = $db->getConnection();
         $this->circlesPdo = new CirclesPdo($pdo);
+        $this->memberController= new MembersController();
     }
       public function is_exist($circleId):bool{
           return $this->circlesPdo->is_exist($circleId);
       }
     function processRequest($method,$userId,$id,$data){
           if($method=="DELETE"&&empty($data)&&isset($userId)&&isset($id)){
-            $this->delete_circle($id);
+            $this->delete_circle($id,$userId);
           }
     }
 
-    public function delete_circle($circleId){
+    public function delete_circle($circleId,$userId){
        if(!$this->circlesPdo->is_exist($circleId)){
         HttpResponse::send(404,null,["error"=>"Not found,Check circle id"]);
+        return;
+       }
+       if($this->memberController->get_member_role($userId)!='Admin'){
+        HttpResponse::send(404,null,["error"=>"You are not allowed,you are not the admin"]);
         return;
        }
        $success=$this->circlesPdo->delete_circle($circleId);
