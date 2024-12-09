779994048947fd3a53cf8cf7a2a3a8b459aaa393
roqia <elmaghrabyroqia2003@gmail.com>
2024-12-09 05:57:07 +0200

    feat:Add approve_request function to update status of pending request

--- backend/pdos/RequestsPdo.php
+++ backend/pdos/RequestsPdo.php
@@ -13,7 +13,7 @@ class RequestsPdo{
     public function get_pending_requests($circleId):array{
         try{
           $sql="SELECT r.id,r.userId,u.name,u.education,u.brief,u.createdAt FROM requests r INNER JOIN users u
-          ON u.id=r.userId ";
+          ON u.id=r.userId AND r.status='Pending";
           $stm=$this->pdo->prepare($sql);
           $stm->execute();
           $requests=$stm->fetchAll(PDO::FETCH_ASSOC);
@@ -22,10 +22,24 @@ class RequestsPdo{
             HttpResponse::send(500, null, ["error" => "Internal server error"]);
             exit;
         }
+    }
+        public function approve_request($requestId):bool{
+            
+                 $sql="UPDATE requests 
+                 SET status =:val
+                 WHERE id=:requestId";
+                 $stm=$this->pdo->prepare($sql);
+                 return $stm->execute([
+                    ':val'=>'Approved',
+                    ':requestId'=>$requestId
+                 ]);
+
+    }
         
     }
 
-}
+
+
 
 
 
