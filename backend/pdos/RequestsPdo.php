6f29da402c7944bc7ce84e54371559a27b98b40c
roqia <elmaghrabyroqia2003@gmail.com>
2024-12-09 05:34:17 +0200

    fix:Edit sql query to show pending requests only

--- backend/pdos/RequestsPdo.php
+++ backend/pdos/RequestsPdo.php
@@ -13,7 +13,7 @@ class RequestsPdo{
     public function get_pending_requests($circleId):array{
         try{
           $sql="SELECT r.id,r.userId,u.name,u.education,u.brief,u.createdAt FROM requests r INNER JOIN users u
-          ON u.id=r.userId ";
+          ON u.id=r.userId AND r.status='Pending";
           $stm=$this->pdo->prepare($sql);
           $stm->execute();
           $requests=$stm->fetchAll(PDO::FETCH_ASSOC);
