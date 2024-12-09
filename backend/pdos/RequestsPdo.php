00f99892dcf3ef3ad6c1bee741c875b58e69f729
roqia <elmaghrabyroqia2003@gmail.com>
2024-12-09 03:21:31 +0200

    feat:Add get_pending_requests function that returns an array of requests in form of json objects of specific circle id

--- backend/pdos/RequestsPdo.php
+++ backend/pdos/RequestsPdo.php
@@ -10,6 +10,20 @@ class RequestsPdo{
         $this->pdo=$pdo;
         
     }
+    public function get_pending_requests($circleId):array{
+        try{
+          $sql="SELECT r.id,r.userId,u.name,u.education,u.description,u.createdAt FROM requests r INNER JOIN users u
+          ON u.id=r.userId ";
+          $stm=$this->pdo->prepare($sql);
+          $stm->execute();
+          $requests=$stm->fetchAll(PDO::FETCH_ASSOC);
+          return $requests;
+        }catch(PDOException $e){
+            HttpResponse::send(500, null, ["error" => "Internal server error"]);
+            exit;
+        }
+        
+    }
 
 }
 
