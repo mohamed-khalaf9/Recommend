82f04a7db172645a3445370826df1432bbe90e03
mnw2637@gmail.com <mnw2637@gmail.com>
2024-12-10 01:56:06 +0200

    updates in header then adding  middle, footer and add recommendation sections

---
+++ frontend/circleHomePage.js
@@ -0,0 +1,13 @@
+// const rec = document.getElementById("mid");
+// var txt = "";
+// for (let i = 0; i < 4; i++){
+//     txt +='<div class="recoms"><div class="content"><h1>Someone</h1> <p class="desc">This recommendation about: Lorem ipsum dolor, sit amet consecteturadipisicing elit. Aliquam voluptatibus quidem ipsam hicperspiciatis ullam nam quod reprehenderit earum. Possimus aliasatque a, iusto molestias voluptatum suscipit corporis sed quaerat?</p><span class="share"><button class="link"><i class="fa-solid fa-link"></i></button></span><hr /><div class="likes"><button class="like"><i class="fa-regular fa-thumbs-up"></i></button><p class="counter">12</p></div></div></div>';
+//     txt+='<br/>'
+// }
+// rec.innerHTML=txt;
+
+let btn = document.getElementById('add');
+btn.addEventListener('click', () => {
+    let crea = document.getElementById('create');
+    crea.style.display = 'block';
+})
