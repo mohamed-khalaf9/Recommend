d5219ba14e0515e92461eaa692d57b6781aee812
Mohamed khalaf <149758845+mohamed-khalaf9@users.noreply.github.com>
2024-12-10 19:33:44 +0200

    Update MembersPdo.php

--- backend/pdos/MembersPdo.php
+++ backend/pdos/MembersPdo.php
@@ -8,12 +8,18 @@ class MembersPdo{
         $this->pdo=$pdo;
         
     }
-    public function is_member($userId):bool{
-        $sql="SELECT userId FROM members WHERE userId=:userId";
-        $stm=$this->pdo->prepare($sql);
-        $stm->execute([':userId' => $userId]);
-        return $stm->fetchColumn()!==false;
-      }
+  public function is_member($userId, $circleId): bool {
+    $sql = "SELECT userId 
+            FROM members 
+            WHERE userId = :userId AND circleId = :circleId";
+    $stm = $this->pdo->prepare($sql);
+    $stm->execute([
+        ':userId' => $userId,
+        ':circleId' => $circleId
+    ]);
+    return $stm->fetchColumn() !== false;
+}
+
 
 }
 
