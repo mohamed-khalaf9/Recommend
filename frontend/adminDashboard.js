324f7764ddd86b7a97bbe4b51610df6fb554fd1f
mnw2637@gmail.com <mnw2637@gmail.com>
2024-12-13 16:09:54 +0200

    some updates on admin dashboard page

--- frontend/adminDashboard.js
+++ frontend/adminDashboard.js
@@ -1,33 +1,29 @@
-// let members = document.getElementById("members");
-// let text = "<h2>Members:</h2>";
-// for (let i = 0; i < 15; i++) {
-//   text += `<div class="item">
-//             <span>member x</span>
-//             <input type="checkbox" name="member" class="member" />
-//           </div>`;
-// }
-// members.innerHTML = text;
+let members = document.getElementById("members");
+let text = "<h2>Members:</h2>";
+for (let i = 0; i < 8; i++) {
+  text += `<div class="item${i}">
+            <span class="span${i}">member ${i+1}</span>
+            <button class="remove"><i class="fa-regular fa-circle-xmark"></i></button>
+          </div>`;
+}
+members.innerHTML = text;
 
-// let requests = document.getElementById("requests");
-// let text2 = "<h2>Requests:</h2>";
-// for (let i = 0; i < 10; i++) {
-//   text2 += `<div class="item">
-//             <span>request x</span>
-//             <input type="checkbox" name="accept" class="request" />
-//             <input type="checkbox" name="reject" class="request" />
-//           </div>`;
-// }
-// requests.innerHTML = text2;
+let requests = document.getElementById("requests");
+let text2 = `<h2>Requests:</h2>`;
+for (let i = 0; i < 8; i++) {
+  text2 += `<div class="item">
+            <span>Request x</span>
+            <button class="accept"><i class="fa-regular fa-circle-check"></i></button>
+            <button class="reject"><i class="fa-regular fa-circle-xmark"></i></button>
+          </div>`;
+}
+requests.innerHTML = text2;
 
+const remove = document.querySelectorAll('.remove');
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const checkboxes = document.querySelectorAll(".member");
-//   checkboxes.forEach((checkbox, index) => {
-//     checkbox.addEventListener("change", () => {
-//         if (checkbox.checked) {
-//             checkbox.parentElement.remove();
-//       }
-//     });
-//   });
-// });
-
+remove.forEach(element => {
+  element.addEventListener('click', () => {
+    let parent = element.parentNode
+    parent.remove()
+  })
+});
