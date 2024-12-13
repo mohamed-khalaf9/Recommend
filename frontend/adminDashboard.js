b999c51bce1f2f65418e38311628b7a9751ba947
mnw2637@gmail.com <mnw2637@gmail.com>
2024-12-13 12:28:36 +0200

    adminDashboard structure and style and some test scripts

---
+++ frontend/adminDashboard.js
@@ -0,0 +1,33 @@
+// let members = document.getElementById("members");
+// let text = "<h2>Members:</h2>";
+// for (let i = 0; i < 15; i++) {
+//   text += `<div class="item">
+//             <span>member x</span>
+//             <input type="checkbox" name="member" class="member" />
+//           </div>`;
+// }
+// members.innerHTML = text;
+
+// let requests = document.getElementById("requests");
+// let text2 = "<h2>Requests:</h2>";
+// for (let i = 0; i < 10; i++) {
+//   text2 += `<div class="item">
+//             <span>request x</span>
+//             <input type="checkbox" name="accept" class="request" />
+//             <input type="checkbox" name="reject" class="request" />
+//           </div>`;
+// }
+// requests.innerHTML = text2;
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const checkboxes = document.querySelectorAll(".member");
+//   checkboxes.forEach((checkbox, index) => {
+//     checkbox.addEventListener("change", () => {
+//         if (checkbox.checked) {
+//             checkbox.parentElement.remove();
+//       }
+//     });
+//   });
+// });
+
