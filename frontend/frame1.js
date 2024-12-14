b51901ab44ad387f68ece43d24642d58c691736f
jehad16 <120113059+jehad16@users.noreply.github.com>
2024-12-14 16:12:48 +0200

    Final Edits

--- frontend/frame1.js
+++ frontend/frame1.js
@@ -1,4 +1,10 @@
 function LogOut() {
     window.open('index.html', '_blank');
+     window.close();
+    // localStorage.clear;
+    localStorage.removeItem(token);
+}
+function profile() {
+    window.open ('profile.html', '_blank');
     window.close();
 }
