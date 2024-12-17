828e31ce67791045094b6cf835007ab0d9c6c1af
mnw2637@gmail.com <mnw2637@gmail.com>
2024-12-17 19:02:36 +0200

    commit

--- frontend/adminDashboard.js
+++ frontend/adminDashboard.js
@@ -1,84 +1,59 @@
-// let members = document.getElementById("members");
-// let text = "<h2>Members:</h2>";
-// for (let i = 0; i < 8; i++) {
-//   text += `<div class="item${i}">
-//             <span class="span${i}">member ${i+1}</span>
-//             <button class="remove"><i class="fa-regular fa-circle-xmark"></i></button>
-//           </div>`;
-// }
-// members.innerHTML = text;
-
-// let requests = document.getElementById("requests");
-// let text2 = `<h2>Requests:</h2>`;
-// for (let i = 0; i < 8; i++) {
-//   text2 += `<div class="item">
-//             <span>Request x</span>
-//             <button class="accept"><i class="fa-regular fa-circle-check"></i></button>
-//             <button class="reject"><i class="fa-regular fa-circle-xmark"></i></button>
-//           </div>`;
-// }
-// requests.innerHTML = text2;
-
-// const remove = document.querySelectorAll('.remove');
-
-// remove.forEach(element => {
-//   element.addEventListener('click', () => {
-//     let parent = element.parentNode
-//     parent.remove()
-//   })
-// });
-let token = localStorage.getItem('token');
+let token = localStorage.getItem("token");
 let circleId = localStorage.getItem("circleID");
 function getMembersOfCircle() {
   let url = `http://localhost/Recommend/backend/members/${circleId}`;
   fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      Authorization: `Bearer ${token}`
-    }
-  }).then(response => {
-    if (response.ok) return response.json()
-    else {
-      let membersList = document.getElementById('members')
-      membersList.innerHTML = 'No members joined to this circle.';
-      membersList.style.color='red'
-    }
-  }).then(data => {
-    data.forEach(member => {
-      let text = "";
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      if (response.ok) return response.json();
+      else {
+        let membersList = document.getElementById("members");
+        membersList.innerHTML = "No members joined to this circle.";
+        membersList.style.color = "red";
+      }
+    })
+    .then((data) => {
+      data.forEach((member) => {
+        let text = "";
         text += ` <li>
                    <span>${member.name}</span>
-                    <button class="remove">
+                    <button class="remove" onclick="removeMember(${memberId})">
                       <i class="fa-regular fa-circle-xmark"></i>
                     </button>
                   </li>`;
+      });
+      let membersList = document.getElementById("members");
+      membersList.innerHTML = text;
     })
-    let membersList = document.getElementById("members");
-    membersList.innerHTML = text;
-  }).catch(error=>{
-    console.log(error);
-  })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function getPendingRequests() {
   let url = `http://localhost/Recommend/backend/requests/${circleId}`;
   fetch(url, {
-    Method: 'GET',
-    Headers:
-    {
+    Method: "GET",
+    Headers: {
       Authorization: `${token}`,
-    }
-  }).then(response => {
-    if (response.ok) return response.json();
-    else {
-      let requestsList = document.getElementById("requests");
-      requestsList.innerHTML = "there are no pending requests.";
-      requestsList.style.color = "red";
-    }
-  }).then(data => {
-    data.forEach((request) => {
-      let text = "";
-      text += ` <li class="item">
+    },
+  })
+    .then((response) => {
+      if (response.ok) return response.json();
+      else {
+        let requestsList = document.getElementById("requests");
+        requestsList.innerHTML = "there are no pending requests.";
+        requestsList.style.color = "red";
+      }
+    })
+    .then((data) => {
+      data.forEach((request) => {
+        let text = "";
+        text += ` <li class="item">
             <span>Request x</span>
             <button class="accept">
               <i class="fa-regular fa-circle-check"></i>
@@ -87,11 +62,34 @@ function getPendingRequests() {
               <i class="fa-regular fa-circle-xmark"></i>
             </button>
           </li>`;
+      });
+      let membersList = document.getElementById("members");
+      membersList.innerHTML = text;
     });
-    let membersList = document.getElementById("members");
-    membersList.innerHTML = text;
-  })
 }
-document.addEventListener('DOMContentLoaded', () => {
-  getMembersOfCircle()
-})
+
+function removeMember(memberId) {
+  fetch(`http://localhost/Recommend/backend/members/${memberId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ circleId }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (response.ok) {
+        alert(data.message);
+        fetchMembers();
+      } else {
+        alert("Error: " + data.message);
+      }
+    })
+    .catch((error) => console.error("Error removing member:", error));
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  getMembersOfCircle();
+});
